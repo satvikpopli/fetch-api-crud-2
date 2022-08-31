4e6c7e77bfae36927225d5769bc9f93de7e484e0
Satvik Popli <satvikpopli4@gmail.com>
2022-09-01 05:05:58 +0530

    Finish project

--- script.js
+++ script.js
@@ -1,108 +1,116 @@
+"use strict";
+
 const url = "https://630a8ccaf280658a59d0126b.mockapi.io/posts";
 
 let saveButton = document.querySelector(".save");
 
+function cleanPostsArea() {
+    postsArea.innerHTML = "";
+}
 
 // DISPLAY POSTS
 const postsArea = document.querySelector(".posts");
 const template = document.querySelector("template");
 const form = document.querySelector("form");
 
+async function display() {
+    cleanPostsArea();
+    const response = await fetch(url);
+    const allPosts = await response.json();
+    allPosts.forEach(post => {
+        const clone = template.content.cloneNode(true);
+        const card = clone.querySelector(".card");
+        card.querySelector(".card-title").textContent = post.title;
+        card.querySelector(".card-text").textContent = post.content;
+        card.id = post.id;
+        postsArea.prepend(clone);
+
+        const deleteBtn = card.querySelector(".delete");
+        deleteBtn.addEventListener("click", deletePost.bind(card));
+
+        const editBtn = card.querySelector(".edit");
+        editBtn.addEventListener("click", editPost.bind(card));
+    });
+}
+
+display();
 
 
 
 
+// ADD POSTS
+async function addPost(title, content) {
+
+}
+
+const addPostButton = document.querySelector(".submit");
+const titleInput = document.querySelector("#title");
+const contentInput = document.querySelector("#content");
+addPostButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const title = titleInput.value;
+    const content = contentInput.value;
+    const newPost = {
+        'title': `${title}`,
+        'content': `${content}`
+    }
+    await fetch(url,
+        {
+            method: 'POST',
+            body: JSON.stringify(newPost),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    display();
+    titleInput.value = "";
+    contentInput.value = "";
+})
 
 
+// DELETE POSTS
+async function deletePost() {
+    await fetch(`${url}/${this.id}`, {
+        method: 'DELETE'
+    });
+    display();
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// async function fetchPosts() {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     return data;
-// }
-
-// const template = document.querySelector("template");
-
-
-// // GET POSTS
-
-// const postsArea = document.querySelector(".posts");
-
-// async function displayPosts() {
-//     const allPosts = await fetchPosts();
-//     allPosts.forEach(post => {
-//         const card = template.content.querySelector(".card").cloneNode(true);
-//         card.querySelector(".card-title").textContent = post.title;
-//         card.querySelector(".card-text").textContent = post.content;
-//         card.id = post.id;
-//         const deletebtn = card.querySelector(".delete");
-//         deletebtn.addEventListener("click", (e) => {
-//             deletePost(e.target.closest(".card").id);
-//             e.target.closest(".card").remove();
-//         });
-//         postsArea.appendChild(card);
-//     })
-// }
-
-// displayPosts();
-
-// // ADD POSTS
-// const submitButton = document.querySelector(".submit");
-// submitButton.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     const title = document.querySelector("#title").value;
-//     const content = document.querySelector("#content").value;
-//     const newPost = { "title": `${title}`, "content": `${content}` };
-//     fetch(url, {
-//         method: "POST",
-//         body: JSON.stringify(newPost),
-//         headers: {
-//             "Content-Type": "application/json"
-//         }
-//     })
-//     displayPosts();
-// })
-
-// // DELETE POSTS
-// async function deletePost(id) {
-//     await fetch(`${url}/${id}`, {
-//         method: "DELETE"
-//     })
-// }
-
-
-// // EDIT POSTS
-// // editButton.addEventListener("click", (e) => {
-// //     e.preventDefault();
-// //     const title = document.querySelector("#title").value;
-// //     const content = document.querySelector("#content").value;
-// //     const newPost = { "title": `${title}`, "content": `${content}` };
-// //     fetch(url, {
-// //         method: "PUT",
-// //         body: JSON.stringify(newPost),
-// //         headers: {
-// //             "Content-Type": "application/json"
-// //         }
-// //     })
-// //     displayPosts();
-// // })
-
-
+// EDIT POSTS
+async function editPost() {
+    const anchor = document.querySelector("#anchor")
+    const title = this.querySelector(".card-title").textContent;
+    const content = this.querySelector(".card-text").textContent;
+    titleInput.value = title;
+    contentInput.value = content;
+    let saveButtonNew = saveButton.cloneNode(true);
+    saveButton.remove();
+    saveButton = saveButtonNew;  // clone and replace to get rid of all previous eventListeners
+    addPostButton.style.display = "none";
+    form.appendChild(saveButton);
+    saveButton.style.display = "inline";
+    anchor.scrollIntoView();
+    saveButton.addEventListener("click", async (e) => {
+        e.preventDefault();
+        const title = titleInput.value;
+        const content = contentInput.value;
+        const newPost = {
+            'title': `${title}`,
+            'content': `${content}`
+        }
+        await fetch(`${url}/${this.id}`,
+            {
+                method: 'PUT',
+                body: JSON.stringify(newPost),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+        titleInput.value = "";
+        contentInput.value = "";
+        addPostButton.style.display = "inline";
+        saveButton.style.display = "none";
+        display();
+    })
+}
